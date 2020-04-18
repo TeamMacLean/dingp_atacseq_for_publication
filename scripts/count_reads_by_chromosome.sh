a803125c3320d954349f4fbdd038950f87c3eb55
tsl-ramkrishna <ram_krishna.shrestha@tsl.ac.uk>
2020-04-18 16:36:32 +0100

    script to count reads by chromosome

---
+++ scripts/count_reads_by_chromosome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source samtools-1.9
+
+bam=$1
+
+echo "Chr1 $(samtools view $bam Chr1 | wc -l) "
+echo "Chr2 $(samtools view $bam Chr2 | wc -l) "
+echo "Chr3 $(samtools view $bam Chr3 | wc -l) "
+echo "Chr4 $(samtools view $bam Chr4 | wc -l) "
+echo "Chr5 $(samtools view $bam Chr5 | wc -l) "
+echo "mitochondria $(samtools view $bam mitochondria | wc -l) "
+echo "chloroplast $(samtools view $bam chloroplast | wc -l) "
