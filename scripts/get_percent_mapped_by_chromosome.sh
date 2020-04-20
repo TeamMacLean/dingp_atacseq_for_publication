c40106bf836b64d5d85dc7b87bc5fb0a9c1e577c
tsl-ramkrishna <ram_krishna.shrestha@tsl.ac.uk>
2020-04-20 13:39:47 +0100

    scripts updated to get paired end data analysis and merge chromosome columns

--- scripts/get_percent_mapped_by_chromosome.sh
+++ scripts/get_percent_mapped_by_chromosome.sh
@@ -1,1 +1,13 @@
-echo "Sample,TotalSeqs,Chr1,Chr2,Chr3,Chr4,Chr5,mitochondria,chloroplast" > percent_aligned_by_chromosome.csv;  for bam in  /tsl/scratch/shrestha/pingtao/dingp_Ath_ATACseq_2019_1/wt*_sorted.bam /tsl/scratch/shrestha/pingtao/dingp_Ath_ATACseq_2019_1/seti[^gh]*_sorted.bam   ; do base=$(basename $bam | sed 's/_sorted.bam//;s/_/ /g' | awk 'BEGIN{OFS="_"}{$NF="";  print $0}' | sed 's/_$//');  total=$(grep ^Total /tsl/scratch/shrestha/pingtao/dingp_Ath_ATACseq_2019_1/results/fastqc/$base/$base/*R1_fastqc/fastqc_data.txt | awk '{print $3}'); echo $base $total  $(cat ${bam}.chr.count.txt | awk -v total=$total '{print (($2/total)*100)/2}') ; done | sed 's/ /,/g' >> percent_aligned_by_chromosome.csv
+#!/bin/bash
+
+# getting percentage of reads aligned to chromosomes, mitochondria, chloroplast
+
+echo "Sample,TotalPESeqs,Chr1-5,mitochondria,chloroplast" > analysis/percent_aligned_by_chromosome.csv; 
+for bam in  /tsl/scratch/shrestha/pingtao/dingp_Ath_ATACseq_2019_1/wt*_sorted.bam /tsl/scratch/shrestha/pingtao/dingp_Ath_ATACseq_2019_1/seti[^gh]*_sorted.bam   ; do
+	base=$(basename $bam | sed 's/_sorted.bam//;s/_/ /g' | awk 'BEGIN{OFS="_"}{$NF="";  print $0}' | sed 's/_$//');
+	total=$(grep ^Total /tsl/scratch/shrestha/pingtao/dingp_Ath_ATACseq_2019_1/results/fastqc/$base/$base/*R1_fastqc/fastqc_data.txt | awk '{print $3}'); 
+	chr1_5_pe=$(cat ${bam}.chr.count.txt | grep ^Chr | awk -v total=$total 'BEGIN{sum=0}{sum+=$2}END{print ((sum/2)/total)*100}')  # dividing by 2 to get total paired end reads
+	mitochondria=$(cat ${bam}.chr.count.txt | grep mitochondria | awk -v total=$total '{print (($2/2)/total)*100}')  # dividing to get paired end reads
+	chloroplast=$(cat ${bam}.chr.count.txt | grep chloroplast | awk -v total=$total '{print (($2/2)/total)*100}')   # dividing to get paired end reads
+	echo $base,$total,${chr1_5_pe},$mitochondria,$chloroplast ;
+done  >> analysis/percent_aligned_by_chromosome.csv
--- scripts/get_total_mapped_by_chromosome.sh
+++ scripts/get_total_mapped_by_chromosome.sh
@@ -1,1 +1,15 @@
-echo "Sample,TotalSeqs,Chr1,Chr2,Chr3,Chr4,Chr5,mitochondria,chloroplast" > total_aligned_by_chromosome.csv;  for bam in  /tsl/scratch/shrestha/pingtao/dingp_Ath_ATACseq_2019_1/wt*_sorted.bam /tsl/scratch/shrestha/pingtao/dingp_Ath_ATACseq_2019_1/seti[^gh]*_sorted.bam   ; do base=$(basename $bam | sed 's/_sorted.bam//;s/_/ /g' | awk 'BEGIN{OFS="_"}{$NF="";  print $0}' | sed 's/_$//');  total=$(grep ^Total /tsl/scratch/shrestha/pingtao/dingp_Ath_ATACseq_2019_1/results/fastqc/$base/$base/*R1_fastqc/fastqc_data.txt | awk '{print $3}'); echo $base $total  $(cat ${bam}.chr.count.txt | awk -v total=$total '{print $2/2}') ; done | sed 's/ /,/g' >> total_aligned_by_chromosome.csv
+#!/bin/bash
+
+# Getting total Paired End reads in chromosomes, chloroplast and mitochondria
+
+echo "Sample,TotalPESeqs,Chr1-5,mitochondria,chloroplast" > analysis/total_aligned_by_chromosome.csv;
+
+for bam in  /tsl/scratch/shrestha/pingtao/dingp_Ath_ATACseq_2019_1/wt*_sorted.bam /tsl/scratch/shrestha/pingtao/dingp_Ath_ATACseq_2019_1/seti[^gh]*_sorted.bam   ; do
+	base=$(basename $bam | sed 's/_sorted.bam//;s/_/ /g' | awk 'BEGIN{OFS="_"}{$NF="";  print $0}' | sed 's/_$//');
+	total=$(grep ^Total /tsl/scratch/shrestha/pingtao/dingp_Ath_ATACseq_2019_1/results/fastqc/$base/$base/*R1_fastqc/fastqc_data.txt | awk '{print $3}');
+
+	chr1_5_pe=$(cat ${bam}.chr.count.txt | grep ^Chr | awk 'BEGIN{sum=0}{sum+=$2}END{print sum/2}')  # dividing by 2 to get total paired end reads
+	mitochondria=$(cat ${bam}.chr.count.txt | grep mitochondria | awk '{print $2/2}')  # dividing to get paired end reads
+	chloroplast=$(cat ${bam}.chr.count.txt | grep chloroplast | awk '{print $2/2}')   # dividing to get paired end reads
+	echo $base,$total,${chr1_5_pe},$mitochondria,$chloroplast ;
+done >> analysis/total_aligned_by_chromosome.csv
